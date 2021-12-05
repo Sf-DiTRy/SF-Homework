9c888045c11f238361edab9bb3fbd11976d684fa
DiTRy <sf.ditry@gmail.com>
2021-12-05 23:12:41 +0300

    IOS15-17_Module

---
+++ IOS15-17_Module/IOS15-17_Module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS15-17_Module
+//
+//  Created by DiTRy on 05.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
