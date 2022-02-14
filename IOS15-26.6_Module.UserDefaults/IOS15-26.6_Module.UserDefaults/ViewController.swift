272e5731bf5ce08dbf841e6fecfcf8ffbf6c71da
DiTRy <sf.ditry@gmail.com>
2022-02-14 21:44:56 +0300

    IOS15-26.6_Module.UserDefaults
    NOT complete. In progress...

---
+++ IOS15-26.6_Module.UserDefaults/IOS15-26.6_Module.UserDefaults/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS15-26.6_Module.UserDefaults
+//
+//  Created by DiTRy on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
