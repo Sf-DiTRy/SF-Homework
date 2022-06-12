d9a9c594ab2870f9aaba8cff97c59a4841287041
DiTRy <sf.ditry@gmail.com>
2022-06-12 23:57:55 +0300

    IOS15-37.7_Module.Serialized&Deserialized.Final Project

---
+++ IOS15-37.7_Module.Serialized&Deserialized.Final Project/IOS15-37.7_Module.Serialized&Deserialized.Final Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS15-37.7_Module.Serialized&Deserialized.Final Project
+//
+//  Created by DiTRy on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
